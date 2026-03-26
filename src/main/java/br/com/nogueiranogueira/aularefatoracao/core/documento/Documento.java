710d910d060d9ed32c1206da90af2a20a7e05c05
Gustavo Domingos Cortelassi <gustavo.cortelassi@bpkedu.com.br>
2026-03-25 21:18:46 -0300

    adicionando NIF

--- src/main/java/br/com/nogueiranogueira/aularefatoracao/core/documento/Documento.java
+++ src/main/java/br/com/nogueiranogueira/aularefatoracao/core/documento/Documento.java
@@ -1,5 +1,5 @@
 package br.com.nogueiranogueira.aularefatoracao.core.documento;
 
-public sealed interface Documento permits Cpf, Curp, Ssn {
+public sealed interface Documento permits Cpf, Curp, Ssn, Nif {
     String valor();
 }
---
+++ src/main/java/br/com/nogueiranogueira/aularefatoracao/core/documento/Nif.java
@@ -0,0 +1,5 @@
+package br.com.nogueiranogueira.aularefatoracao.core.documento;
+
+public record Nif(String valor) implements Documento {
+    
+}
